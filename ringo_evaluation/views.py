4d409d44c5f2e2b7cb0842519112645e74602616
Torsten Irländer <torsten.irlaender@intevation.de>
2014-11-27 17:18:38 +0100

    Use RecursiveRelationExporter for evaluation.

--- ringo_evaluation/views.py
+++ ringo_evaluation/views.py
@@ -11,7 +11,7 @@ from ringo.views.request import (
     is_confirmed,
     get_item_from_request
 )
-from ringo.lib.imexport import Exporter
+from ringo.lib.imexport import RecursiveRelationExporter
 from ringo.lib.helpers.appinfo import get_app_location
 from ringo.lib.helpers.format import get_local_datetime
 from ringo.views.base.create import create
@@ -90,36 +90,28 @@ def _handle_evaluation_request(request, items):
         # 1. Load evaluation file
         factory = Extension.get_item_factory()
         evaluation = factory.load(form.data["evaluations"])
+        export_config = evaluation.configuration
         spreadsheet = ezodf.opendoc(evaluation.data)
 
         # 2. Export data
-        exporter = Exporter(clazz)
-        export = exporter.perform(items)
+        exporter = RecursiveRelationExporter(clazz, export_config)
+        sheet_data = exporter.perform(items)
+        relation_config = exporter.get_relation_config()
 
-        # 4. Create sheets per relation
-        sheetname = modul.name
+        # 3. Create sheets per relation
         sheets = spreadsheet.sheets
-        try:
-            sheet = sheets[sheetname]
-        except:
-            sheets.append(ezodf.Table(sheetname))
-            sheet = sheets[sheetname]
-
-        if len(export) > 0:
-            fields = sorted(export[0].keys())
-            sheet.reset(size=(len(export)+1, len(fields)))
-            for x, field in enumerate(fields):
-                cell = sheet[0,x]
-                cell.set_value(field)
-        else:
-            fields = []
-        for y, element in enumerate(export):
-            for x, field in enumerate(fields):
-                cell = sheet[y+1, x]
-                try:
-                    cell.set_value(element[field] or "")
-                except:
-                    log.exception("Can not find field on export")
+        for relation in relation_config:
+            data = sheet_data[relation]
+            if relation == "root":
+                sheetname = modul.name
+            else:
+                sheetname = relation
+            try:
+                sheet = sheets[sheetname]
+            except:
+                sheets.append(ezodf.Table(sheetname))
+                sheet = sheets[sheetname]
+            _fill_sheet(sheet, data, relation_config[relation])
 
         # 4. Build response
         ef = "ods"
@@ -139,6 +131,20 @@ def _handle_evaluation_request(request, items):
         rvalue['dialog'] = renderer.render(items)
         return rvalue
 
+
+def _fill_sheet(sheet, data, fields):
+    if len(data) > 0:
+        if not fields:
+            fields = sorted(data[0].keys())
+    sheet.reset(size=(len(data)+1, len(fields)))
+    for x, field in enumerate(fields):
+        cell = sheet[0,x]
+        cell.set_value(field)
+    for y, item in enumerate(data):
+        for x, field in enumerate(fields):
+            cell = sheet[y+1, x]
+            if field in item:
+                cell.set_value(item[field] or "")
 
 # Register the view and request handlers.
 set_web_action_view("evaluate", evaluate)
